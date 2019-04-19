8fcf9093ca2a90eb21735251e508e4303234a9d8
Felix Morau <felixmorau@gmail.com>
2019-04-19 22:07:31 +0200

    movieLibrary finished

--- src/views/MoviesLibrary.vue
+++ src/views/MoviesLibrary.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="Movies">
     <movies-component />
+    <movies-library />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import MoviesComponent from "@/components/Movies/Movies.vue"
+import MoviesLibrary from "@/components/Movies/MoviesLibrary.vue"
 
 export default {
   name: 'movies',
   components: {
-    MoviesComponent
+    MoviesComponent,
+    MoviesLibrary
   }
 }
 </script>
